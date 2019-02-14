b6b2478472669ef17ca6689621c2ad5048728f52
manu9812 <32127007+manu9812@users.noreply.github.com>
2019-02-14 13:26:38 -0500

    CALCULADORA
    calculadora con interfaz grafica en eclipse java

---
+++ Calculadora/src/Calculadora/Main.java
@@ -0,0 +1,14 @@
+package Calculadora;
+
+public class Main {
+
+	/**
+	 * @param args the command line arguments
+	 * Manuela Garcia Monsalve 
+	 * 10 febrero 2019
+	 */
+	public static void main(String[] args) {
+		InterfazCalculadora calculadora = new InterfazCalculadora();
+		calculadora.setVisible(true);
+	}
+}
